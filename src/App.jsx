f2dc046a9612023948e696376632c8af75c85a0d
abbiejonnes <abigaelmwangi534@gmail.com>
2026-07-05 21:02:07 +0300

    fix:update the app.jsx file by aupdating the footer route

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,43 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
+
 import Navbar from "./components/Navbar";
-import ProtectedRoute from "./components/ProtectedRoute";
-import Cart from "./components/Cart";
-import ProductCard from "./components/ProductCard";
-import ProductList from "./components/ProductList";
 import Footer from "./components/Footer";
+import ProtectedRoute from "./components/ProtectedRoute";
+
+import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import ProductDetails from "./pages/ProductDetails";
 import Checkout from "./pages/Checkout";
+import ProductDetails from "./pages/ProductDetails";
+
+import ProductList from "./components/ProductList";
+import Cart from "./components/Cart";
 
 function App() {
-return (
-  <BrowserRouter>
-  <Navbar/>
-  <Footer>
-  <Routes>
-    <Route path="/" element={<Home/>}/>
-    <Route path="/Cart" element={<Cart/>}/>
-    <Route path="/ProductCard" element={<ProductCard/>}/>
-    <Route path="/ProductList" element={<ProductList/>}/>
-    <Route path="/Login" element={<Login/>}/>
-    <Route path="/Register" element={<Register/>}/>
-    <Route path="/ProductDetails" element={<ProductDetails/>}/>
-    <Route
-  path="/checkout"element={<ProtectedRoute><Checkout /></ProtectedRoute> }/>
-  </Routes>
-  </Footer>
-  </BrowserRouter>
-)
+  return (
+    <BrowserRouter>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<ProductList />} />
+        <Route path="/products/:id" element={<ProductDetails />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route
+          path="/checkout"
+          element={
+            <ProtectedRoute>
+              <Checkout />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+
+      <Footer />
+    </BrowserRouter>
+  );
 }
+
 export default App;
